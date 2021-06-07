a6ff268ad9c722baf2d05cb87d42e2fc760fc17c
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-06-07 20:38:07 +0530

    Reports Database Added

--- src/reports/models.py
+++ src/reports/models.py
@@ -8,3 +8,8 @@ class Report(models.Model):
 	image = models.ImageField(upload_to='reports',blank=True)
 	remarks = models.TextField()
 	author = models.ForeignKey(Profile,on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.name)

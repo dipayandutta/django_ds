6399a05f1e4215f1f68df989a025ab9c65c85049
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-06-08 22:04:04 +0530

    Sales Class based view added

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home_view
+from .views import (
+	home_view, 
+	SalesListView,
+	)
 
 app_name = 'sales'
 
 urlpatterns = [
-	path('',home_view,name='home')
+	path('',home_view,name='home'),
+	path('list/',SalesListView.as_view(),name='list'), # need to add .as_view() for class based view
 ]
--- src/sales/views.py
+++ src/sales/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Sale
 # Create your views here.
 def home_view(request):
 	hello = 'This is after all troubleshooting done!'
-	return render(request,'sales/main.html',{'hello':hello})
+	return render(request,'sales/home.html',{'hello':hello})
+
+
+
+# Class Based View
+class SalesListView(ListView):
+	model = Sale
+	template_name = 'sales/main.html'
+	
